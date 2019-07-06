40d27398f4871985695c986fdd14f4709a2aaa20
Evan <evantyoverdy95@gmail.com>
2019-07-06 11:45:54 +0700

    Top Navigation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TopNav from "./TopNav";
 
 function App() {
   return (
-    <div className="App">
-        <p>Contact Me</p>
-        <div>
+      <div>
+          <TopNav/>
+        <div className="App">
+            <p>Contact Me</p>
+            <div>
 
-        <form action="http://localhost:3001/savecontact">
-        <label>Name</label>
-        <input type="text" id="fname" name="firstname" placeholder="Your name.."/><br/>
+                <form action="http://localhost:3001/savecontact">
+                    <label>Name</label>
+                    <input type="text" id="fname" name="firstname" placeholder="Your name.."/><br/>
 
-        <label>Email</label>
-        <input type="email" id="email" name="email" placeholder="Your email"/><br/>
+                    <label>Email</label>
+                    <input type="email" id="email" name="email" placeholder="Your email"/><br/>
 
-        <label>Comment</label>
-        <input type="comment" id="comment" name="comment" placeholder="Your comment"/><br/>
+                    <label>Comment</label>
+                    <input type="comment" id="comment" name="comment" placeholder="Your comment"/><br/>
 
-        <input type="submit" value="Submit"/>
-        </form>
+                    <input type="submit" value="Submit"/>
+                </form>
 
+            </div>
         </div>
-    </div>
+      </div>
   );
 }
 
